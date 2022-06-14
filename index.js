6d03b4b5ff926986e5bc6326e63fe3cc74dfc74b
managment_system <mas152q@gmail.com>
2022-06-14 16:09:48 +1000

    nodejs

---
+++ index.js
@@ -0,0 +1,88 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+  'What is your project title?',
+  'What is the description of this project?',
+  'What installation steps are required to run this?',
+  'What instructions or examples are there using the project? (To add a screenshot, copy and paste the following and edit where necessary: ![alt text](assets/images/screenshot.png)',
+  'What license does it use?',
+  'Are there any contributing guidelines?',
+  'What tests have you written to be able to run from the readme? You must write them here.',
+  'What is your GitHub username? (Your Github profile will be added automatically)',
+  'What is your email address? (To contact you.)',
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Markdown file generated')
+  );
+}
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: questions[0],
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: questions[1],
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: questions[2],
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: questions[3],
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: questions[4],
+        choices: [
+          'No License used.',
+          'MIT License',
+          'Apache License 2.0',
+          'Mozilla Public License 2.0',
+          'GNU GPLv3',
+        ],
+      },
+      {
+        type: 'list',
+        name: 'contribute',
+        message: questions[5],
+        choices: [
+          'Contributing guidelines: [Code of conduct](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)',
+          'To be decided on at a later date.',
+        ],
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: questions[6],
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: questions[7],
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: questions[8],
+      },
+    ])
+    .then((data) => writeToFile('README.md', markdown.generateMarkdown(data))); // making use of first-class functions: to use a function (i.e. generateMarkdown(data)) as the parameter inside another function.
+}
+
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,103 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license.includes('MIT')) {
+    return '![MIT License](https://img.shields.io/badge/License-MIT-red)';
+  } else if (license.includes('Apache')) {
+    return '![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-yellow)';
+  } else if (license.includes('Mozilla')) {
+    return '![Mozilla Public License 2.0](https://img.shields.io/badge/License-Mozilla%20Public%202.0-yellowgreen)';
+  } else if (license.includes('GNU')) {
+    return '![GNU General Public License v3.0](https://img.shields.io/badge/License-GNU%20General%20Public%20v3.0-blue)';
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license.includes('MIT')) {
+    return '[MIT License](https://choosealicense.com/licenses/mit/)';
+  } else if (license.includes('Apache')) {
+    return '[Apache 2.0 License](https://choosealicense.com/licenses/apache-2.0/)';
+  } else if (license.includes('Mozilla')) {
+    return '[Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)';
+  } else if (license.includes('GNU')) {
+    return '[GNU General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/)';
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license.includes('No License used.')) {
+    return `# License
+    
+  Licensed under the `;
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const badge = renderLicenseBadge(data.license); // calls the function inside the generateMarkdown function to return a const
+
+  const link = renderLicenseLink(data.license); // calls the function inside the generateMarkdown function to return a const
+
+  const section = renderLicenseSection(data.license); // calls the function inside the generateMarkdown function to return a const
+
+  if (section === '') {
+    licenseShortcut = '';
+  } else {
+    licenseShortcut = '- [License](#license)';
+  }
+
+  return `# ${data.title}
+
+  ${badge}
+
+  # Description
+  
+  ${data.description}
+
+  # Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${licenseShortcut}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  # Installation
+  
+  ${data.install}
+
+  # Usage
+  
+  ${data.usage}
+  
+  ${section}${link}
+
+  # Contributing
+  
+  ${data.contribute}
+
+  # Tests
+  
+  ${data.test}
+
+  # Questions
+  
+  You can find my GitHub profile at: https://github.com/${data.username}
+
+  If you have any questions, you can contact me by email: ${data.email}
+
+`;
+}
+
+module.exports = { generateMarkdown }; // right hand side of module.exports requires the use of an object to get module.exports to work correctly, functions do not need their parameters written here.
